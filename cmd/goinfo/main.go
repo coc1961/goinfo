295588e2ec50160beaca6a8e03bd5a13976aba75
Carlos Castro <carlos_o_castro@yahoo.com.ancestor>
2024-03-05 21:53:00 -0300

    Fix

--- cmd/goinfo/main.go
+++ cmd/goinfo/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/coc1961/goinfo/internal/callhierarchy"
 )
@@ -12,11 +13,28 @@ func main() {
 	src := flag.String("s", "", "source go file")
 	line := flag.Int("l", 0, "line number")
 	col := flag.Int("c", 0, "column number")
+	find := flag.String("f", "", "function name with parameters")
 	flag.Parse()
 
-	if *src == "" || *line == -1 || *col == -1 {
+	if *src == "" {
 		flag.CommandLine.Usage()
 		os.Exit(1)
+	}
+	if (*line == -1 || *col == -1) && *find == "" {
+		flag.CommandLine.Usage()
+		os.Exit(1)
+	}
+
+	if *find != "" {
+		b, _ := os.ReadFile(*src)
+		arr := strings.Split(string(b), "\n")
+		for i, s := range arr {
+			idx := strings.Index(s, find)
+			if idx >= 0 {
+				*line = i + 1
+				*col = idx + 1
+			}
+		}
 	}
 
 	a, err := callhierarchy.New().Parse(*src, *line, *col)
